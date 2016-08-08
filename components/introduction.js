78e660e340c2561362393e580011a3977453787d
Mick O'Brien <mickaobrien@gmail.com>
2016-08-08 12:47:48 +0100

    update intro - closes #53

--- components/introduction.js
+++ components/introduction.js
@@ -24,21 +24,19 @@ var IntroductionScreen = React.createClass({
         </View>
         <View style={styles.textcontent}>
           <Text style={styles.paragraph}>
-            Echoes from the Past is a location triggered audio guide which uses the findings of the 2009 Report of the Commission to Inquire into Child Abuse (the Ryan Report) to explore the former site of St. Vincent’s Industrial School.
+            Echoes from the Past is a location triggered audio guide which uses the findings of the 2009 Report of the Commission to Inquire into Child Abuse (the Ryan Report) to explore the former site of St. Vincent’s Industrial School. The recommended starting point is the Drimnagh stop on the Luas Red Line.
           </Text>
           <Text style={styles.paragraph}>
-              St. Vincent’s Industrial School, more commonly known as “Goldenbridge”, was an industrial school in Dublin run by the Sisters of Mercy. Originally founded as a rehabilitation service for female prisoners, the site was converted to an industrial school for girls in 1883. The number of residents fluctuated over the decades reaching a peak of 193 in 1964. The school closed in 1983 and the buildings have since been demolished. Experiences of ex-residents of Goldenbridge featured in a number of television and radio programmes such as ‘Dear Daughter’ and ‘States of Fear’ and some ex-residents were prominent in the campaign for redress. The screening of these programmes provoked a huge public reaction, and was followed by an apology by the Taoiseach and the establishment of the Commission to Inquire into Child Abuse in 2000.
+            St. Vincent’s Industrial School, more commonly known as “Goldenbridge”, was an industrial school in Dublin run by the Sisters of Mercy. Originally founded as a rehabilitation service for female prisoners, the site was converted to an industrial school for girls in 1883. The number of residents fluctuated over the decades reaching a peak of 193 in 1964. The school closed in 1983 and the buildings have since been demolished. Experiences of ex-residents of Goldenbridge featured in a number of television and radio programmes such as ‘Dear Daughter’ and ‘States of Fear’ and some ex-residents were prominent in the campaign for redress. The screening of these programmes provoked a huge public reaction, and was followed by an apology by the Taoiseach and the establishment of the Commission to Inquire into Child Abuse in 2000.
           </Text>
           <Text style={styles.paragraph}>
-              This audio guide is designed to give the user a sense of the common experiences had by ex-residents of the industrial school using direct testimonial statements from the Ryan Report. The Report is publicly accessible at the following link: www.childabusecommission.ie
+            This audio guide is designed to give the user a sense of the common experiences had by ex-residents of the industrial school using direct testimonial statements from the Ryan Report. The Report is publicly accessible at  <Link url='www.childabusecommission.ie' text= 'childabusecommission.ie' />.
           </Text>
           <Text style={styles.paragraph}>
-            Echoes from the Past is part of a two-year project Industrial Memories analysing the
-            findings of the 2009 Ryan Report funded by the Irish Research Council under New Horizon 2015
-            (for further information see the <Link style={styles.paragraph} url='http://irishmemorystudies.com/index.php/industrial-memories/' text='Industrial Memories website' />).
+            Echoes from the Past was created by Maeve Casserly, Tom Lane and Mick O'Brien. It is part of a two-year project Industrial Memories analysing the findings of the 2009 Ryan Report funded by the Irish Research Council under New Horizon 2015 (for further information see the <Link style={styles.paragraph} url='http://irishmemorystudies.com/index.php/industrial-memories/' text='Industrial Memories website' />).
           </Text>
           <Text style={styles.paragraph}>
-            For more information on Goldenbridge Industrial School and a downloadable version of the audio see please visit ......(WORDPRESS website which Maeve will create this week)
+            For more information on Goldenbridge Industrial School and a downloadable version of the audio see please visit <Link style={styles.paragraph} url='https://echoesfromthepast.org/' text='echoesfromthepast.org' />.
           </Text>
           <TouchableHighlight style={styles.button}
             onPress={this.startApp}>
