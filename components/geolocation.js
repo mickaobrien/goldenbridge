8513f22976be73f341f36d2c9d6061e69c8bd996
Mick O'Brien <mickaobrien@gmail.com>
2016-08-09 16:43:13 +0100

    use watchposition for android, setinterval getpos for ios - closes #47

--- components/geolocation.js
+++ components/geolocation.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import TimerMixin from 'react-timer-mixin';
+import {Platform} from 'react-native';
 
 var Geolocation = React.createClass({
   mixins: [TimerMixin],
@@ -25,21 +26,32 @@ var Geolocation = React.createClass({
     );
   },
 
+  watchPosition: function() {
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      this.props.onPositionUpdate(position);
+      this.setState({position});
+    },
+    (error) => {
+      console.log('watchPosition error: ' + error.message);
+    },
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 2}
+                                                      );
+  },
+
   componentDidMount: function() {
-    this.setInterval(this.getPosition, 500);
-    //this.watchID = navigator.geolocation.watchPosition((position) => {
-        //this.props.onPositionUpdate(position);
-        //this.setState({position});
-      //},
-      //(error) => {
-        //console.log('watchPosition error: ' + error.message);
-      //},
-      //{enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 2}
-    //);
+    // Hacky way to get iOS to update position regularly
+    // TODO fix!
+    if (Platform.OS === 'ios') {
+      this.setInterval(this.getPosition, 500);
+    } else {
+      this.watchPosition();
+    }
   },
 
   componentWillUnmount: function() {
-    navigator.geolocation.clearWatch(this.watchID);
+    if (this.watchID) {
+      navigator.geolocation.clearWatch(this.watchID);
+    }
   },
 
   render: function() {
